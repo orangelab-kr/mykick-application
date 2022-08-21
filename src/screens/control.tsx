a232ae4616f3d760ce3f8c57dfacbc3ea40f8d39
bakjonghun <bakjonghun@hikick.kr>
2022-08-20 20:42:27 -0400

    add: control page with real kickboard control

--- src/screens/control.tsx
+++ src/screens/control.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import {Switch, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
 import NaverMapView, {MapType} from 'react-native-nmap';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components';
 import {BasicInfo} from '../components/BasicInfo/BasicInfo';
 import {BasicInfoUnit} from '../components/BasicInfo/BasicInfoUnit';
-import {CommonText} from '../components/Common/CommonText';
 import {CoreController} from '../components/CoreController/CoreController';
 import {PowerButton} from '../components/PowerButton/PowerButton';
 import {SpeedController} from '../components/SpeedController/SpeedController';
+import {ConnectStatusBadge} from '../components/StatusBadge/ConnectStatusBadge';
 import {StatusBadge} from '../components/StatusBadge/StatusBadge';
 import {StatusBadgeGroup} from '../components/StatusBadge/StatusBadgeGroup';
 import {StickyController} from '../components/StickyController/StickyController';
 import {SwitchController} from '../components/SwitchController/SwitchController';
 import {TopBar} from '../components/TopBar/TopBar';
-import {screenHeight, screenWidth} from '../tools/screenSize';
+import {useKickboard} from '../tools/kickboard';
 
 export const Control: React.FC = () => {
+  const kickboard = useKickboard({
+    secretKey: 'string',
+    macAddress: 'string',
+    serviceId: 'string',
+    characteristics: {
+      writeId: 'string',
+      readId: 'string',
+    },
+  });
+
+  useEffect(() => {
+    kickboard.connect();
+  }, []);
+
+  console.log(kickboard.power);
+
   return (
     <SafeAreaView>
       <TopBar />
       <Container>
         <StatusBadgeGroup>
-          <StatusBadge color="#58C948" icon="circle" label="연결됨" />
+          <ConnectStatusBadge status={kickboard.status} />
           <StatusBadge color="#3578F6" icon="umbrella" label="26º" />
         </StatusBadgeGroup>
         <NaverMapView
@@ -43,9 +58,27 @@ export const Control: React.FC = () => {
             <BasicInfoUnit icon="bolt" label="96%" />
           </BasicInfo>
           <CoreController>
-            <SwitchController label="라이트" />
-            <PowerButton />
-            <SwitchController label="배터리 잠금" />
+            <SwitchController
+              label="라이트"
+              disabled={kickboard.status !== 'connected'}
+              value={kickboard.light}
+              onChange={() => {
+                kickboard.switchLightOn();
+              }}
+            />
+            <PowerButton
+              onPress={kickboard.switchPowerOn}
+              color={kickboard.power ? '#3578F6' : '#153B54'}
+              disabled={kickboard.status !== 'connected'}
+            />
+            <SwitchController
+              label="배터리 잠금"
+              disabled={kickboard.status !== 'connected'}
+              value={kickboard.batteryLock}
+              onChange={() => {
+                kickboard.switchBatteryLock();
+              }}
+            />
           </CoreController>
           <SpeedController />
         </StickyController>
