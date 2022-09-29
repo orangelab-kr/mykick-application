a8ae3adcf60384d8b34c4a5a8e3996e01c8d647a
bakjonghun <bakjonghun@hikick.kr>
2022-09-29 21:24:03 +0900

    refactor: stable for new version & remove menu

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,39 +1,13 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {MenuAction, MenuView} from '@react-native-menu/menu';
 import React from 'react';
 import {View} from 'react-native';
 import styled from 'styled-components';
-import {navigationRef} from '../../tools/navigation';
 import {screenHeight, screenWidth} from '../../tools/screenSize';
 import {TopBarLogo} from './TopBarLogo';
-import {TopBarSettingsButton} from './TopBarSettingsButton';
 
 export const TopBar: React.FC = () => {
-  const route = navigationRef.current?.getCurrentRoute();
-  const actions: MenuAction[] = [
-    {
-      id: 'Control',
-      title: '리얼마이킥 모드',
-      state: route?.name === 'Control' ? 'on' : 'off',
-    },
-    {
-      id: 'Rent',
-      title: '마이킥 모드',
-      state: route?.name === 'Rent' ? 'on' : 'off',
-    },
-  ];
-
-  const onAction = (e: any) =>
-    navigationRef.current?.navigate(e.nativeEvent.event);
-
   return (
     <TopContainer>
       <TopBarLogo />
-      {/* <MenuView
-        actions={actions}
-        onPressAction={onAction}
-        {...({children: <TopBarLogo />} as any)}
-      /> */}
       {/* <TopBarSettingsButton onPress={() => AsyncStorage.clear()} /> */}
     </TopContainer>
   );
